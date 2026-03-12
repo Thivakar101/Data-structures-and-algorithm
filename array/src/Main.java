4a8780939d8178aa6daafe5c9b7fc4d95657c9c2
Thivakar101 <thivakartab@gmail.com>
2026-03-12 19:08:44 +0530

    array initaialization and insertiona dn deletion

---
+++ array/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,0};
+
+
+
+//        //insertion in a position
+//        int position=0;
+//        int value=67;
+//        for(int i=arr.length-1;i>position;i--){
+//            arr[i]=arr[i-1];
+//
+//        }
+//        arr[position]=value;
+
+
+
+
+//        deletion in a array
+//        int position=0;
+//        for(int i=position;i< arr.length-1;i++){
+//            arr[i]=arr[i+1];
+//        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+}
