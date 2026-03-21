20b0659929dbd40ba5c7bb8f137b5336eb8f16b3
Thivakar101 <thivakartab@gmail.com>
2026-03-21 21:18:38 +0530

    test run2

--- test run/src/Main.java
+++ test run/src/Main.java
@@ -1,15 +1,17 @@
 class Node{
     int data;
     Node next;
+    Node previous;
      Node(int val){
          this.data=val;
          this.next=null;
+         this.previous=null;
      }
 
 }
-class Sll{
+class cll {
     private Node first,last;
-    public  Sll(){
+    public cll(){
         first=null;
         last=null;
     }
@@ -25,11 +27,54 @@ class Sll{
             last=newnode;
         }
     }
+    public void print(){
+        Node temp=first;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    public void removefront(){
+        if(first==null) return;
+        first=first.next;
+        return;
+    }
+    public void removelast(){
+        if(first== last){
+            first=null;
+            last=null;
+            return;
+        }
+        Node temp=first;
+        while(temp!=last){
+            temp=temp.next;
+        }
+        temp.next=null;
+        temp=last;
+        return;
+
+    }
+    public void remove(int position){
+        if(position==0){
+            removefront();
+            return;
+        }
+        Node temp=first;
+        for(int i=0;i<position-1;i++){
+            temp=temp.next;
+
+        }
+        if(temp.next==last){
+            temp=last;
+        }
+        temp.next=temp.next.next;
+    }
+
 }
 public class Main {
     public static void main(String[] args) {
-        Sll fsli=new Sll();
-        fsli.add(10);
+        cll fcli =new cll();
+       fcli.add(10);
 
     }
 }
