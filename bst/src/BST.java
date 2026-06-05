f8058e1d95a5932dfd674f50780cb130ed14fb09
Thivakar101 <thivakartab@gmail.com>
2026-06-05 22:52:00 +0530

    working with the bst and orders

--- bst/src/BST.java
+++ bst/src/BST.java
@@ -25,4 +25,41 @@ class Binarysearchtree{
         }
         return root;
     }
+    public boolean search(int data){
+        return recsearch(root,data);
+    }
+    private boolean recsearch(Node root,int data){
+        if(root==null){
+            return false;
+        }  if (root.data==data) {
+            return true;
+        } if (data>root.data) {
+            return recsearch(root.right,data);
+        }
+        return recsearch(root.left,data);
+
+    }
+    public void inorder(){
+        recinorder(root);
+    }
+    private void recinorder(Node root){
+        if(root!=null){
+            recinorder(root.left);
+            System.out.println(root.data+" ");
+            recinorder(root.right);
+        }
+
+    }
+    public void preorder(){
+        recpreorder(root);
+    }
+    private void recpreorder(Node root){
+        if(root!=null){
+
+            System.out.println(root.data+" ");
+            recpreorder(root.left);
+            recpreorder(root.right);
+        }
+
+    }
 }
